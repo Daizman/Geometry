549a82ca441257a34b7d1c68cc756353dcc52c44
Daizman <42967651+Daizman@users.noreply.github.com>
2022-08-23 14:44:53 +0500

    Тестовое по MS SQL

---
+++ SQLTask.sql
@@ -0,0 +1,7 @@
+﻿--Products(id, name)
+--Categories(id, name)
+--ProductCategories(productId, categoryId)  many to many
+select pr.Name as 'Имя продукта', cat.Name as 'Имя категории'
+from Products pr
+left join ProductCategories prCat on pr.Id = prCat.ProductId
+left join Categories cat on prCat.CategoryId = cat.Id;
